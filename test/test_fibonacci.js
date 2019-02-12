4d8be635e1da4d0ef29c55a0b36f89f4407ae42f
Chris Gianelle <cgianelle@gmail.com>
2019-02-11 22:49:35 -0500

    Added recursive API to the service

--- test/test_fibonacci.js
+++ test/test_fibonacci.js
@@ -2,18 +2,34 @@ const fibonacci = require('../lib/fibonacci');
 const expect = require('chai').expect;
 
 describe('Fibonacci Tests', ()=>{
-    it('Should verify the sequence values for a given term', ()=>{
-        expect(fibonacci.fibonacci(0)).to.equal(0);
-        expect(fibonacci.fibonacci(1)).to.equal(1);
-        expect(fibonacci.fibonacci(2)).to.equal(1);
-        expect(fibonacci.fibonacci(3)).to.equal(2);
-        expect(fibonacci.fibonacci(4)).to.equal(3);
-        expect(fibonacci.fibonacci(5)).to.equal(5);
-        expect(fibonacci.fibonacci(6)).to.equal(8);
-        expect(fibonacci.fibonacci(7)).to.equal(13);
-        expect(fibonacci.fibonacci(8)).to.equal(21);
-        expect(fibonacci.fibonacci(9)).to.equal(34);
-        expect(fibonacci.fibonacci(19)).to.equal(4181);
-        expect(fibonacci.fibonacci(20)).to.equal(6765);
+    it('Should verify the sequence values for a given term with the goldenRatioFibonacci function', ()=>{
+        expect(fibonacci.goldenRatioFibonacci(0)).to.equal(0);
+        expect(fibonacci.goldenRatioFibonacci(1)).to.equal(1);
+        expect(fibonacci.goldenRatioFibonacci(2)).to.equal(1);
+        expect(fibonacci.goldenRatioFibonacci(3)).to.equal(2);
+        expect(fibonacci.goldenRatioFibonacci(4)).to.equal(3);
+        expect(fibonacci.goldenRatioFibonacci(5)).to.equal(5);
+        expect(fibonacci.goldenRatioFibonacci(6)).to.equal(8);
+        expect(fibonacci.goldenRatioFibonacci(7)).to.equal(13);
+        expect(fibonacci.goldenRatioFibonacci(8)).to.equal(21);
+        expect(fibonacci.goldenRatioFibonacci(9)).to.equal(34);
+        expect(fibonacci.goldenRatioFibonacci(19)).to.equal(4181);
+        expect(fibonacci.goldenRatioFibonacci(20)).to.equal(6765);
     });
+
+    it('Should verify the sequence values for a given term with the recursiveFibonacci function', ()=>{
+        expect(fibonacci.recursiveFibonacci(0)).to.equal(0);
+        expect(fibonacci.recursiveFibonacci(1)).to.equal(1);
+        expect(fibonacci.recursiveFibonacci(2)).to.equal(1);
+        expect(fibonacci.recursiveFibonacci(3)).to.equal(2);
+        expect(fibonacci.recursiveFibonacci(4)).to.equal(3);
+        expect(fibonacci.recursiveFibonacci(5)).to.equal(5);
+        expect(fibonacci.recursiveFibonacci(6)).to.equal(8);
+        expect(fibonacci.recursiveFibonacci(7)).to.equal(13);
+        expect(fibonacci.recursiveFibonacci(8)).to.equal(21);
+        expect(fibonacci.recursiveFibonacci(9)).to.equal(34);
+        expect(fibonacci.recursiveFibonacci(19)).to.equal(4181);
+        expect(fibonacci.recursiveFibonacci(20)).to.equal(6765);
+    });
+
 });
--- test/test_route.js
+++ test/test_route.js
@@ -8,6 +8,8 @@ let app = express();
 const router = require('../lib/route').router;
 chai.use(chaiHttp);
 
+const apis = ["/fibonacci", "/fibonacci/recursive"];
+
 describe('Fibonacci Router Tests', function() {
     before(()=>{
         app.use('/',router);
@@ -15,8 +17,9 @@ describe('Fibonacci Router Tests', function() {
     });
 
     it('Should return a fibonacci value for a given term', ()=>{
-        return chai.request(app)
-            .get('/fibonacci?term=5')
+        const promises = apis.map(api => {
+            return chai.request(app)
+            .get(api+'?term=5')
             .then((res) => {
                 expect(res).to.have.status(200);
                 expect(res).to.have.property('body');
@@ -24,30 +27,46 @@ describe('Fibonacci Router Tests', function() {
                 expect(term).to.equal('5');
                 expect(value).to.equal(5);
             });
+        });
+
+        return Promise.all(promises);
     });
 
     it('Should return a 400 error when no query is provided', ()=>{
-        return chai.request(app)
-            .get('/fibonacci')
+
+        const promises = apis.map(api => {
+            return chai.request(app)
+            .get(api)
             .then((res) => {
                 expect(res).to.have.status(400);
             });
+        });
+
+        return Promise.all(promises);
     });
 
     it('Should return a 400 error when query is NaN', ()=>{
-        return chai.request(app)
-            .get('/fibonacci?term=help')
+        const promises = apis.map(api => {
+            return chai.request(app)
+            .get(api+'?term=help')
             .then((res) => {
                 expect(res).to.have.status(400);
             });
+        });
+
+        return Promise.all(promises);
     });
 
     it('Should return a 400 error when query does not include "term"', ()=>{
-        return chai.request(app)
-            .get('/fibonacci?team=5')
+        const promises = apis.map(api => {
+            return chai.request(app)
+            .get(api+'?team=5')
             .then((res) => {
                 expect(res).to.have.status(400);
             });
+        });
+
+        return Promise.all(promises);
     });
 
 });
