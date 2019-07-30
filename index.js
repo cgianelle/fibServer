e2c015f1c329e3ea295cc5fd7ae0fd95470b004f
Chris Gianelle <cgianelle@gmail.com>
2019-07-29 23:06:55 -0400

    Playing around with secrets

--- index.js
+++ index.js
@@ -8,4 +8,7 @@ const port = config.get('server.port');
 
 app.use('/',router);
 
-app.listen(port, () => console.log(`Fibonacci Server Listening on ${port}!`))
+app.listen(port, () => {
+    console.log(`Hello, ${config.get('firstName')} ${config.get('lastName')}`);
+    console.log(`Fibonacci Server Listening on ${port}!`);
+});
